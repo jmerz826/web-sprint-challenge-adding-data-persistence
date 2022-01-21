680c35ea451a948568f328258515dc41ded78b48
John Merz <jmerz826@gmail.com>
2022-01-21 11:19:01 -0500

    resources enpoints built

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,19 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const getById = function (id){
+    return db('resources').where('resource_id', id)
+}
+
+const getAll = () => {
+    return db('resources')
+}
+
+const addResource = async (newResource) => {
+    const [id] = await db('resources').insert(newResource)
+    
+    return getById(id)
+}
+
+
+module.exports = {getById, addResource, getAll}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,11 +1,28 @@
 const router = require('express').Router()
+const Resource = require('./model')
 
 router.post('/', (req, res, next) => {
-
+    Resource.addResource(req.body)
+        .then(newR => {
+            res.status(201).json(newR)
+        })
+        .catch(err => next(err))
 })
 
 router.get('/', (req, res, next) => {
-    
+    Resource.getAll()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(err => next(err))
+})
+
+router.get('/:id', (req, res, next) => {
+    Resource.getById(req.params.id)
+        .then(r => {
+            res.status(200).json(r)
+        })
+        .catch(err => next(err))
 })
 
 
