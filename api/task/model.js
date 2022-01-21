e62b227f08a9302bb90b643944c00736ba4faafd
John Merz <jmerz826@gmail.com>
2022-01-21 18:08:52 -0500

    all tests passing

--- api/task/model.js
+++ api/task/model.js
@@ -1,13 +1,23 @@
 const db = require('../../data/dbConfig')
 
-const getAll = () => {
-    return db('tasks as t')
+const getAll = async () => {
+    const all = await db('tasks as t')
         .leftJoin('projects as p', 't.project_id', 'p.project_id')
         .select('t.*', 'p.project_name', 'p.project_description')
+    
+    const mapped = all.map(task => {
+        return ({
+            ...task,
+            task_completed: Boolean(task.task_completed)
+        })
+    })
+    return mapped
 }
 
-const getById = (id) => {
-    return db('tasks').where('task_id', id)
+const getById = async (id) => {
+    let [x] = await db('tasks').where('task_id', id)
+    x = {...x, task_completed: Boolean(x.task_completed)}
+    return x
 }
 
 const create = async (newTask) => {
